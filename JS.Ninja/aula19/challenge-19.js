28d597319342c1bc85b5d99a8a3ba31cae554d4c
lmoraesdev <lbc_moraes@outlook.com>
2022-02-21 00:35:02 -0300

    finished the exercise

--- JS.Ninja/aula19/challenge-19.js
+++ JS.Ninja/aula19/challenge-19.js
@@ -26,9 +26,7 @@
 	no console:
 	*/
 	console.log( 'Regex para números usando o construtor:' );
-	var justNumbersRegex = function(text){
-		return text.match(/(^\d+)/gm);
-	}
+	var justNumbersRegex = /^\d+/gm
 
 	/*
 	Verifique se a regex acima casa com o texto na variável `text`, mostrando o
@@ -37,7 +35,7 @@
 	*/
 	var text = '10 anos.\n50 discos vendidos.\nE nem 10% dos meus amigos o conhece.';
 	console.log( '\nNúmeros no início da linha do texto:\n' + text, '\n' );
-	console.log(justNumbersRegex(text));
+	console.log( text.match( justNumbersRegex ) );
 
 	/*
 	- Crie uma regex que case com números no final de uma string. Atribua a
@@ -48,9 +46,7 @@
 	Mostre a regex no console:
 	*/
 	console.log( '\nRegex para números somente no final das linhas:' );
-	var numbersAtTheEnd = function(text){
-		return text.match(/(\d+$)/gm);
-	};
+	var numbersAtTheEnd = /\d+$/gm
 
 	/*
 	Verifique se a regex acima casa com o texto na variável `otherText`,
@@ -60,7 +56,7 @@
 	*/
 	var otherText = 'Silvio Santos, nome artístico de Senor Abravanel (Rio de Janeiro, 12\n de dezembro de 1930), é um apresentador de televisão e empresário brasileiro.\n Proprietário do Grupo Silvio Santos, que inclui empresas como a Liderança\n Capitalização (administradora da loteria Tele Sena), a Jequiti Cosméticos e o\n Sistema Brasileiro de Televisão (mais conhecido como SBT), Silvio Santos possui\n um patrimônio avaliado em aproximadamente 6\n bilhões de reais.';
 	console.log( '\nNúmeros no final da linha:\n\n', otherText, '\n' );
-	console.log( numbersAtTheEnd(otherText) );
+	console.log( otherText.match( numbersAtTheEnd ) );
 
 	/*
 	Vamos criar um método que vai testar se uma classe CSS existe em uma
@@ -87,9 +83,13 @@
 	*/
 	var markup = '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
 	console.log( '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
-	function hasClass( markUp, cssClass ){
-		
-	}
-	console.log();
+	function hasClass( markup, cssClass ){
+		var regex = new RegExp('class=["\'](?:[\\w\\s]+)?' + cssClass + '(?:[\\w\\s]+)?["\']', 'g');
+		return regex.test(markup);
 
-}());
+	}
+	var classes = ['container', 'text', 'date', 'excerpt' , 'main'];
+	classes.forEach( function( cssClass ){
+		console.log( hasClass( markup, cssClass) + ' para a classe ' + cssClass);
+	});
+})();
